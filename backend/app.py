80d303ed4a5f8d0b7d7802cc938e73ddc535cfb2
woragis <masteringthecode.woragis+github@gmail.com>
2024-09-25 19:58:48 -0300

    mend

--- backend/app.py
+++ backend/app.py
@@ -2,12 +2,4 @@ from fastapi import FastAPI
 from backend.routes.document_routes import router
 
 app = FastAPI()
-
 app.include_router(router)
-
-# Database connection
-# MongoDB
-# client = motor.motor_asyncio.AsyncIOMotorClient(environ["MONGODB_URL"])
-
-# db = client.get_database("college")
-# student_collection = db.get_collection("students")
