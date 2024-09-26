9fd894d516363ec4dc44322c769c0b7ff1dc22b6
woragis <masteringthecode.woragis+github@gmail.com>
2024-09-26 02:48:26 -0300

    Update packages

--- frontend/src/pages/pagesData.tsx
+++ frontend/src/pages/pagesData.tsx
@@ -1,8 +1,10 @@
 import { PagesInterface } from "../types/pages";
 import Home from "./Home";
+import Documents from "./Documents";
 
 const pagesData: PagesInterface[] = [
   { name: "home", path: "/", element: <Home /> },
+  { name: "documents", path: "/documents", element: <Documents /> },
 ];
 
 export default pagesData;
--- frontend/src/styles/GlobalStyles.ts
+++ frontend/src/styles/GlobalStyles.ts
@@ -1,11 +1,14 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyles = createGlobalStyle`
+  #root {
+    width: 100%;
+  }
   * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-    font-family: 'Courier New', Courier, monospace;
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
     outline: none;
     text-decoration: none;
     list-style-type: none;
